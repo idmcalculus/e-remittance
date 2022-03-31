1329fbd711ab1fb51918425d617bad30ec5686c5
Damilola Michael Ige <idm.calculus@gmail.com>
2022-03-31 12:53:27 +0100

    make it better

--- app.js
+++ app.js
@@ -18,15 +18,11 @@ var app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// view engine setup
-app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, 'public')));
+// app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/attendance', attendanceRouter);
@@ -41,11 +37,15 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// view engine setup
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = err // req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
